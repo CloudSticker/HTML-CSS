7f78d0c921d10f7296028d9c1320f525fb494806
Cloud Maker <dan9154634874@gmail.com>
2021-03-01 20:01:23 +0300

    Идентефицировал папку с лабой

--- Magick/JSelement.js
+++ Magick/JSelement.js
@@ -6,110 +6,29 @@
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
+
 ctx.lineWidth = 2;
-var posX = 10;
-var posY = 10;
-var i = 0;
-var n = 256;
-var k = true;
-var k3 = true;
-var k2 = true;
-ctx.fillStyle = "#ffff00";
-ctx.strokeStyle = "#ffff00";
-document.querySelector(".JSElement").style.backgroundColor = "#00FFFF"; 
+ctx.fillStyle = "#FF33FF";
+ctx.strokeStyle = "#FF33FF";
+document.querySelector(".JSElement").style.backgroundColor = "#FFFFFF"; 
+var PosX = 0;
+var PosY = 0;
 
-setInterval(function () 
-{
+canvas.style.backgroundColor = "#FFFFFF";
 
-    ctx.clearRect(0, 0, 885, 615);
 
-    ctx.beginPath();
-    ctx.arc(posX, posY, 10, 0, 2 * Math.PI, false);    
-    ctx.fillStyle =  "#" +  n.toString(16) + (255 - n).toString(16)  + (255).toString(16);
-	ctx.strokeStyle =  "#" +  n.toString(16) + (255 - n).toString(16)  + (255).toString(16);
-    ctx.fill();
-	ctx.stroke();
 
-    if (n == 256){
-        k3 = false;
-    }else if (n == 0){
-        k3 = true;
-    }
-
-    if (k3){
-        n++;
-    }else{
-        n--;
-    }
     
-    if (posX == 290){
-        k1 = false
-    }else if (posX == 10){
-        k1 = true;
-    }
-        
-
-    if (k1){
-        posX++;
-    }else{
-        posX--;
-    }
-
-
-    if (posY == 140){
-        k2 = false
-    }else if (posY == 10){
-        k2 = true;
-    }
-        
-
-    if (k2){
-        posY++;
-    }else{
-        posY--;
-    }
-
-    // console.log(document.getElementById("canvas").clientHeight);
-    // console.log(document.getElementById("canvas").clientWidth);
-}
-, 20)
-
-
-
-
-
-function setFadeColor(i, element){
-        var t =  "#" +  i.toString(16) + (255 - i).toString(16)  + (255).toString(16);
-        document.querySelector(element).style.backgroundColor = t; 
-}
-
-
-
 let timerId = setInterval( () => 
 {
-    if (document.querySelector("#gradient_check_box").checked){
-        
-        //console.log("#" +  r.toString(16) + ' ' +  g.toString(16)  + ' ' + b.toString(16));
-
-        //console.log(i);
-
-        if (i == 256){
-            k = false;
-        }else if (i == 0){
-            k = true;
-        }
-
-        if (k){
-            i++;
-        }else{
-            i--;
-        }
-
-        setFadeColor(i, ".JSElement"); 
-        
-    }
+    
+    ctx.beginPath();
+    ctx.moveTo(50, 100);
+    ctx.lineTo(PosX, PosY);
+    ctx.stroke();
+    PosX += 1.5;
 }
  , 20);
 
 
-
+    
