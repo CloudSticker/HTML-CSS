b9ca59252c227904c4b21da1840e356d653345e8
Daniil <dan9154634874@gmail.com>
2021-05-07 21:56:01 +0300

    i create the monster!

--- Magick/scripts/animation.js
+++ Magick/scripts/animation.js
@@ -14,14 +14,28 @@ let header = document.querySelector('header');
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
-
+    const navLinks = document.querySelectorAll('.nav-links li')
+    //Toggle Nav
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index ) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+            }
+        })
+
+        //burg animation
+        burger.classList.toggle('toggle')
+
     });
+
+
 }
 
 navSlide();
-    
+
 
 
 window.addEventListener('scroll', function (){
